0e81020ce1423b71fb47375a7a63edb03ae07bf3
Sree Suryadatta M Vadhoolas <84676910+dattasurya08@users.noreply.github.com>
2021-06-28 21:11:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+    var res = (p * r * y)/100;
+
+    var dt = new Date();
+    var year = dt.getFullYear();
+    year = parseInt(year) + parseInt(y);
+
+    var string = "If you deposit " + p + ",<br/>";
+    string += "at an interest rate of " + r + ".<br/>";
+    string += "You will recieve an amount of " + res + ",<br/>";
+    string += "in the year " + year;
+    document.getElementById("result").innerHTML = string;
 }
-        
+
+function rate_disp()
+{
+  var rate = document.getElementById("rate").value;
+  rate += '%';
+  document.getElementById("rt").innerHTML = rate;
+}
+
+function yr_drop()
+{
+    var str = '';
+    for(i = 1; i <= 100; i++){
+        str += '<option val=' + i + '>' + i + '</option>';
+    }
+    rate_disp();
+    document.getElementById("years").innerHTML = str;
+}
